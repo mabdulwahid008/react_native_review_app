f1b8dc56da7c351e5705ccff91cfc419e5bf28b5
mabdulwahid008 <m.abdulwahid008@gmail.com>
2023-02-07 00:31:06 +0500

    learned: how to use custome fonts

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font'
+import  AppLoading  from 'expo-app-loading'
 
+const getFonts = () =>{ 
+  return Font.loadAsync({
+    'nunito': require('./assets/fonts/Nunito-Bold.ttf')
+  })
+}
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontLooaded, setFontLooaded] = useState(false)
+
+
+  if(fontLooaded)
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    )
+  else
+    return (
+    <AppLoading startAsync={getFonts} onFinish={()=>{setFontLooaded(true)}} onError={()=>{console.log();}}/>
+    )
+
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +34,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontFamily: 'nunito'
+  }
 });
