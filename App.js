64459d3b63c28470c4c43d256cab055b79597c66
mabdulwahid008 <m.abdulwahid008@gmail.com>
2023-02-11 22:17:40 +0500

    global styles

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import React from 'react'
-import {StyleSheet, View, Text} from 'react-native'
+import { View, Text} from 'react-native'
+import { globalStyles } from './styles/globalStyles'
 
 function App() {
+
+  
   return (
-    <View style={StyleSheet.container}>
-      <Text>Hello World</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.titleText}>Hello World</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
-  }
-})
+
 
 export default App
